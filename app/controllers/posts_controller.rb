d503d22419033a1014d6cfa8e8b8d934e54a2bce
mysthkr4036@gmail.com <hikaru_miyashita>
2023-02-17 12:41:59 +0900

    create cretae function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,28 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+  
+  def index
+    @posts = Post.all
+  end
+  
+  def create
+    @post = Post.new(content: params[:post][:content])
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "投稿しました！"
+      else
+        render :new
+      end
+    end
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+  
 end
